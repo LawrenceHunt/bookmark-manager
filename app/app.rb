14c5ecc827d1a4e4d2d8f171160454b652b0c365
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-10-19 12:41:29 +0100

    Tests passing for tag filtering - stage 16

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,10 @@
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
-ENV["RACK_ENV"] ||= "development"
-
 class BookmarkManager < Sinatra::Base
+
   get '/links' do
     @links = Link.all
     erb :'/links/index'
@@ -19,6 +20,12 @@ class BookmarkManager < Sinatra::Base
 
   get '/links/new' do
     erb :'/links/new'
+  end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
   end
 
   # start the server if ruby file executed directly
