db7361bd9051eec458dab4b36a3c27cf4292407f
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-10-19 15:37:16 +0100

    Links now viewable by tag

--- app/app.rb
+++ app/app.rb
@@ -10,13 +10,14 @@ class BookmarkManager < Sinatra::Base
     erb :'/links/index'
   end
 
-  post '/links' do
-    link = Link.new(url: params[:url], title: params[:title])
-    tag = Tag.first_or_create(name: params[:tags])
-    link.tags << tag
-    link.save
-    redirect '/links'
-  end
+    post '/links' do
+      link = Link.new(url: params[:url], title: params[:title])
+      params[:tags].split.each do |tag|
+        link.tags << Tag.create(name: tag)
+      end
+      link.save
+      redirect '/links'
+    end
 
   get '/links/new' do
     erb :'/links/new'
